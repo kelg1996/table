64387fa5fafd8d3ac25cc19580776bfc8e3461db
kelg1996 <46920620+kelg1996@users.noreply.github.com>
2023-01-09 10:06:08 -0500

    Delete src/app directory

--- src/app/table/ColumnDirective.ts
+++
@@ -1,9 +0,0 @@
-import {Directive, ViewContainerRef} from "@angular/core";
-
-@Directive({
-  selector: '[columnHost]'
-})
-export class ColumnDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {
-  }
-}
--- src/app/table/FilterDirective.ts
+++
@@ -1,9 +0,0 @@
-import {Directive, ViewContainerRef} from "@angular/core";
-
-@Directive({
-  selector: '[filterHost]'
-})
-export class FilterDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {
-  }
-}
